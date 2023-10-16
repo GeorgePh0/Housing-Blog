3df257b04a965e421c45624a814de475b007aaf3
GeorgePh0 <george.philip0020@gmail.com>
2023-10-16 17:22:21 +0000

    Add filter, search field, list display to admin panel and approve comments to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,21 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    list_filter = ('status', 'created_on')
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    list_display = ('name', 'body', 'approved', 'created_on', 'post')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+
+        queryset.update(approved=True)
