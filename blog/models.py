208b8b01d40517f0dfb0d1a231ad6eedbd5d036e
GeorgePh0 <george.philip0020@gmail.com>
2023-10-20 13:52:34 +0000

    Add Profile to views.py and urls.py. Add profile.html

--- blog/models.py
+++ blog/models.py
@@ -49,6 +49,7 @@ class Comment(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import CommentForm
 
@@ -76,3 +77,8 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+@login_required
+def Profile(request):
+    return render(request, 'profile.html')
