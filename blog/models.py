e8b21f7f4c7a57f26746ce636a88d4db085fa815
GeorgePh0 <george.philip0020@gmail.com>
2023-10-25 09:44:39 +0000

    Add path for avatar image and create def for saving profile in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from PIL import Image
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -49,8 +50,19 @@ class Comment(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(
+        default='avatar.jpg'
+        upload_to='profile_avatars'
+    )
     bio = models.TextField()
 
     def __str__(self):
-        return str(self.user)
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.avatar.path)
+        if img.height > 300 or img.width 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
